c62cc2131ea9f9357241b2f909ca9d267e89076b
minara35 <minara35@gmail.com>
2020-10-25 11:04:06 -0400

    add new files

--- src/test/java/com/vytrack/step_definitions/CommonStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CommonStepDefinitions.java
@@ -1,13 +1,15 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
 public class CommonStepDefinitions {
+    LoginPage loginPage = new LoginPage();
 
     @Given("user navigates to {string} and {string}")
     public void user_navigates_to_and(String string, String string2) {
-
+        loginPage.navigateTo(string, string2);
     }
 
 
